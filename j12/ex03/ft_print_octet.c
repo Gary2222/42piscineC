ef7e3ef823522655742ef8e217f8c263c6b4ea0a
Gary JEANMAIRE <gjeanmai@dump-mini-2.42.fr>
2016-05-14 21:11:19 +0200

    mise a jour hexdump

--- j12/ex03/ft_print_octet.c
+++ j12/ex03/ft_print_octet.c
@@ -6,7 +6,7 @@
 /*   By: gjeanmai <gjeanmai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/11 23:43:40 by gjeanmai          #+#    #+#             */
-/*   Updated: 2016/05/13 19:03:29 by gjeanmai         ###   ########.fr       */
+/*   Updated: 2016/05/14 21:03:31 by gjeanmai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 void	ft_print_octet(char *octet)
 {
+	int ascii;
+
+	ascii = (int)*octet;
 	ft_putchar(' ');
-//	printf("%d ", octet);
-	ft_putstr(ft_convert_base((octet, DECIMAL, HEXA));
+	if (ascii <= 15)
+		ft_putchar('0');
+	ft_putstr(ft_convert_base(ft_itoa(ascii), DECIMAL, HEXA));
 }
